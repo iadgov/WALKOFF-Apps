d382d002497ffd23903145e422795f5e33c5d4fc
root <root@localhost.localdomain>
2019-01-16 16:32:26 +1100

    Bug hunting

--- Zscaler/app.py
+++ Zscaler/app.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime
 import time
 import requests
 import logging
@@ -9,13 +10,12 @@ logger = logging.getLogger(__name__)
 class Zscaler(App):
     def __init__(self, name, device, context):
         App.__init__(self, name, device, context)
-        self.api_key = self.device.get_encrypted_field('api_key'))
-        self.obfuscated_api_key = self.obfuscate_api_key
+        self.api_key = self.device.get_encrypted_field('api_key')
+        self.obfuscated_api_key = self.obfuscate_api_key()
         self.api_username = self.device_fields['username'],
-        self.api_password = self.device.get_encrypted_field('password'))
-        
-        self.api_host = self.device_fields['zscaler_instance']
-        self.api_base_url = 'https://%s/api/v1/' % (api_host)
+        self.api_password = self.device.get_encrypted_field('password')
+        self.api_host = self.device_fields['zscaler_pop']
+        self.api_base_url = 'https://%s/api/v1/' % (self.api_host)
         self.api_headers = {
             'Content-Type': 'application/json',
             'Cache-Control': 'no-cache'
@@ -32,56 +32,66 @@ class Zscaler(App):
         for j in range(0, len(str(r)), 1):
             key += seed[int(str(r)[j]) + 2]
 
-        return key
+        return key, now
 
     ### Session
     @action
     def create_session(self):
-        url = api_base_url + 'authenticatedSession'
-        data = json.dumps({
-            'apiKey': self.obfuscated_api_key,
-            'username': self.api_username,
-            'password': self.api_key,
-            'timestamp': int(time.time() * 1000)
-        })
-        response = requests.post(url, data=data, headers=api_headers)
-        return response.cookies['JSESSIONID']
+        url = self.api_base_url + 'authenticatedSession'
+        data = {
+            'apiKey': self.obfuscate_api_key()[0],
+            'username': self.api_username[0],
+            'password': self.api_password,
+            'timestamp': self.obfuscate_api_key()[1]
+        }
+        response = requests.post(url, data=json.dumps(data), headers=self.api_headers)
+        print(response.text)
+        print(response.json())
+        print(response.status_code)
+        if response.status_code == 200:
+            return response.cookies['JSESSIONID']
+        else:
+            return  str(datetime.utcnow()), 'Fail'
     
     @action
-    def delete_session(self):
-        headers = api_headersl
+    def delete_session(self, session):
+        headers = self.api_headers
         headers['cookie'] = 'JSESSIONID=%s' % (session)
-        url = api_base_url + 'authenticatedSession'
-        response = requests.delete(url, headers=api_headers)
-        return response.cookies['JSESSIONID']
+        url = self.api_base_url + 'authenticatedSession'
+        response = requests.delete(url, headers=headers)
+        if response.status_code == 200:
+            return 'Successfully logged out'
 
     ### Configuration status
     @action
     def activate_config(self, session):
-        headers = api_headers
+        headers = self.api_headers
         headers['cookie'] = 'JSESSIONID=%s' % (session)
-        url = api_base_url + 'status/activate'
+        url = self.api_base_url + 'status/activate'
         response = requests.post(url, headers=headers)
         return response.json()
     
 
     ### URL Categories
     @action
-    def url_categories(self, session, category_id=None):
-        headers = api_headers
+    def url_categories(self, session, category_id):
+        headers = self.api_headers
         headers['cookie'] = 'JSESSIONID=%s' % (session)
-        url = api_base_url + 'urlCategories/%s' % (category_id)
+        url = self.api_base_url + 'urlCategories/%s' % (category_id)
         response = requests.get(url, headers=headers)
         return response.json()
     
     @action
     def url_lookup(self, session, domain_list):
-        headers = api_headers
-        headers['cookie'] = 'JSESSIONID=%s' % (authenticate())
-        url = api_base_url + 'urlLookup'
+        headers = self.api_headers
+        headers['cookie'] = 'JSESSIONID='+session
+        print(headers)
+        url = self.api_base_url + 'urlLookup'
         data = json.dumps(domain_list)
         response = requests.post(url, data=data, headers=headers)
-        return response.json()
+        print(response.text)
+        json_result = response.json()
+        return json_result
     
     @action
     def add_to_custom_category(self, session, category_id, domain_list):
@@ -93,8 +103,8 @@ class Zscaler(App):
             'dbCategorizedUrls': blocklist,
             'description': category_info['description']
         }
-        headers = api_headers
+        headers = self.api_headers
         headers['cookie'] = 'JSESSIONID=%s' % (session)
-        url = api_base_url + 'urlCategories/%s?action=ADD_TO_LIST' % (category_id)
+        url = self.api_base_url + 'urlCategories/%s?action=ADD_TO_LIST' % (category_id)
         response = requests.put(url, data=json.dumps(data), headers=headers)
         return response.json()
